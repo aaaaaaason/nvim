be69991d9404cbd36a47a709a5a101e624cf376e
aaaaaaason <aaaaaaason@gmail.com>
2023-02-05 17:14:10 +0800

    remove f prefix in telescope custom mapping

--- lua/plugin-config/telescope.lua
+++ lua/plugin-config/telescope.lua
@@ -13,7 +13,11 @@ require('telescope').setup {
     pickers = {
         find_files = {
             hidden = true
+        },
+        buffers = {
+            sort_lastused = true
         }
+
     }
 }
 -- To get fzf loaded and working with telescope, you need to call
@@ -21,8 +25,8 @@ require('telescope').setup {
 require('telescope').load_extension('fzf')
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>s', builtin.grep_string, {})
