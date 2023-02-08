8c452be02494f232e47084fa3ebdf333d1c94e9d
aaaaaaason <aaaaaaason@gmail.com>
2023-02-08 23:59:48 +0800

    adjust harpoon keymaps

--- lua/plugin-config/harpoon.lua
+++ lua/plugin-config/harpoon.lua
@@ -7,7 +7,7 @@ require("harpoon").setup({
 local keymap = vim.keymap
 local kopt = { silent = true, noremap = true }
 
-keymap.set("n", "<leader>a", require("harpoon.mark").toggle_file, kopt)
-keymap.set("n", "<leader>q", require("harpoon.ui").toggle_quick_menu, kopt)
+keymap.set("n", "m", require("harpoon.mark").toggle_file, kopt)
+keymap.set("n", "<leader>a", require("harpoon.ui").toggle_quick_menu, kopt)
 keymap.set("n", "<tab>", require("harpoon.ui").nav_next, kopt)
 keymap.set("n", "<S-tab>", require("harpoon.ui").nav_prev, kopt)
